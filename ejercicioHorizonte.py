c5667abb3a5bc812b13d9a30a3902a0db8112318
JBcruz <jbcm.32@gmail.com>
2024-05-09 14:40:32 -0500

    Primer Commit

---
+++ ejercicioHorizonte.py
@@ -0,0 +1,16 @@
+import test
+
+class Trabajador:
+    def __init__(self, nombre, sueldo_basico, horas_trabajadas, horas_extra, tardanzas, faltas):
+        # Inicializar atributos
+
+    def calcular_sueldo(self):
+        # Calcular sueldo
+
+def main():
+    # Ingreso de datos
+    # Cálculo del sueldo a pagar
+    # Imprimir boleta de pago
+
+if __name__ == "__main__":
+    main()
